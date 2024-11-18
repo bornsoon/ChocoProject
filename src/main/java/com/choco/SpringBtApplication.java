b5ecb6112ea05b9b9d213c5cc5729fdb3015916e
bornsoon <hyolim922@hotmail.com>
2024-11-18 11:13:32 +0900

    start!!!

---
+++ src/main/java/com/choco/SpringBtApplication.java
@@ -0,0 +1,13 @@
+package com.choco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBtApplication.class, args);
+	}
+
+}
