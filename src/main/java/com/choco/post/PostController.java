8a984678fe1fb6080cbc5a7279ce461650d0b26d
bornsoon <hyolim922@hotmail.com>
2024-11-18 11:10:11 +0900

    start!

---
+++ src/main/java/com/choco/post/PostController.java
@@ -0,0 +1,22 @@
+package com.choco.post;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class PostController {
+
+	
+	@GetMapping(value = {"/post", "/post/"})
+	public String home(Model model) {
+		
+		model.addAttribute("serverTime",  "서버시간");
+		
+		return "thymeleaf/choco/post/post_detail";
+	}
+
+}
