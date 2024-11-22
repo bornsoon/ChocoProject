a29762c43e00b0fe6ac345391b91816bbdaef8c6
bornsoon <hyolim922@hotmail.com>
2024-11-22 17:55:26 +0900

    1차 머지(수정중)

---
+++ src/main/java/com/choco/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,42 @@
+package com.choco.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		HttpSession session = request.getSession();
+		String usersId = (String)session.getAttribute("usersId");
+		
+//        if (usersId == null) {
+//            response.sendRedirect("./main_login");
+//            return false;
+//        }
+	
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		//System.out.println("컨트롤러 실행됨.");
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		//System.out.println("뷰가 처리됨.");
+	}
+
+}
