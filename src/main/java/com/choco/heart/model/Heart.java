c5f05066f977bae3bfb7c022a704860ea62fc650
DolMongKim <nevertheless525@gmail.com>
2024-11-22 16:41:50 +0900

    kjk
    qwqwe

---
+++ src/main/java/com/choco/heart/model/Heart.java
@@ -0,0 +1,19 @@
+package com.choco.heart.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Heart {
+
+	private int heartId;  // boardId + replyId (PK)
+	private String usersId;  
+
+}
