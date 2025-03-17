3aafdf77960431e085177c9658365516ac1a97e3
bornsoon <hyolim922@hotmail.com>
2025-03-17 18:41:00 +0900

    첨부파일 model 설정

--- src/main/java/com/choco/attach/model/Attach.java
+++ src/main/java/com/choco/attach/model/Attach.java
@@ -6,13 +6,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter
 //@ToString(exclude= {"attachFile"}) //ToString에서 attachFile만 빼겠다.
+@Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Attach {
 	private int attachId;
 	private String attachName;
-//	private byte[] attachFile;
+ //private byte[] attachFile;
 	private int boardId;
 }
