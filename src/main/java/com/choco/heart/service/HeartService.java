fc43b8bedebc2ff231a0676dcccb226dc2c48afe
bornsoon <hyolim922@hotmail.com>
2025-03-17 18:30:20 +0900

    좋아요 Service Interface 설정

--- src/main/java/com/choco/heart/service/HeartService.java
+++ src/main/java/com/choco/heart/service/HeartService.java
@@ -6,7 +6,7 @@ import com.choco.heart.model.Heart;
 
 public interface HeartService {
     int checkHeart(int heartId, String sessionId); // 특정 게시물 또는 댓글에 좋아요 데이터 존재 여부 확인
-    int getHeartCount(int heartId); // 특정 게시물/댓글 좋아요 개수 반환
+    int getHeartCount(int heartId); // 게시물,댓글 좋아요 개수 반환
     void insertHeart(Heart heart); // 좋아요 추가
     void deleteHeart(Map<String, Object> heartMap); // 좋아요 삭제
 }
