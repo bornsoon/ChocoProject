16a6756af8527efdc512d3fa3c9c8a8a11e6108f
bornsoon <hyolim922@hotmail.com>
2025-03-17 18:27:19 +0900

    좋아요 Repository 설정

--- src/main/java/com/choco/heart/dao/HeartRepository.java
+++ src/main/java/com/choco/heart/dao/HeartRepository.java
@@ -10,8 +10,7 @@ import com.choco.heart.model.Heart;
 @Mapper
 public interface HeartRepository {
    
-    int checkHeart(@Param("heartId") int heartId, // 특정 게시물 또는 댓글에 좋아요 데이터 존재 여부 확인
-				   @Param("sessionId") String sessionId); 
+    int checkHeart(@Param("heartId") int heartId, @Param("sessionId") String sessionId); 
     int getHeartCount(@Param("heartId") int heartId); // 특정 게시물/댓글 좋아요 개수 반환
     void insertHeart(Heart heart); // 좋아요 추가
     void deleteHeart(Map<String, Object> heartMap); // 좋아요 삭제
